95d447f24c0ccea26febf77cb488b9181f9055a9
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-01 06:17:07 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,6 +38,11 @@ class TicTacToe
     user_index.between?(0,8) && !position_taken?(user_index)
   end
 
-
+  def turn_count
+    counter = 0
+    @board.each do |token|
+  if token == "X" || token == "O"
+    counter += 1
+  end
 
 end
