d4ed21a2f5b26e4e926a5c3e46d894eb7fb45627
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-01 06:25:39 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -60,12 +60,15 @@ class TicTacToe
 
   def won?
   WIN_COMBINATIONS.detect do |win_combination|
-    location1 = win_combination[0]
-    location2 = win_combination[1]
-    location3 = win_combination[2]
-    @board[location1] == @board[location2] && @board[location2] == @board[location3] && @board[location1] != " "
+      location1 = win_combination[0]
+      location2 = win_combination[1]
+      location3 = win_combination[2]
+      @board[location1] == @board[location2] && @board[location2] == @board[location3] && @board[location1] != " "
+    end
   end
-end
 
+  def full?
+    @board.none? {|space| space == " "}
+  end
 
 end
